2540ae2cdbd6b7052d24447cf4c5fd98ca6b54d2
Fernando Vizeu Santos <fvizeus@gmail.com>
2017-06-12 04:04:52 -0300

    Using the Cython implementation.

--- exampleXuPayne2009.py
+++ exampleXuPayne2009.py
@@ -2,13 +2,20 @@ import matplotlib
 matplotlib.use("TkAgg")
 import numpy as np
 
-from DEM import DEM
+# Try to use the Cython implementation, fallback to the Python implementation if it doesn't work
+try:
+    from cDEM import DEM
+except:
+    from DEM import DEM
 
 def XuPayne():
-    # Reproducing Xu and Payne 2009
+    """Reproducing Figure 5 of Xu and Payne (2009)"""
+    
     import matplotlib.pyplot as plt
     import Gassmann
     import time
+    
+    # Matrix properties
     # Source: Acoustic Properties of Carbonate Rocks p. 77
     Km_dol = 69.0 # GPa
     Gm_dol = 52.0 # GPa
@@ -17,85 +24,80 @@ def XuPayne():
     Gm_lim = 32.0 # GPa
     rhom_lim = 2.71 # g/cm3
     
+    # Fluid properties
     Kf = 3.0 # GPa
     rhof = 1.0 # g/cm3
     
+    # Porosity
     phimax = 0.4
     
+    # Aspect ratio of different pore types
     alpha_ref = 0.15
     alpha_crack = 0.02
     alpha_stiff = 0.8
     
-    cracks_alphas = []
-    cracks_phis = []
-    for frac in [0.2, 0.4, 0.6, 0.8]:
-        cracks_alphas.append(np.array([alpha_crack, alpha_ref]))
-        cracks_phis.append(np.array([frac*phimax, (1.0-frac)*phimax]))
-    cracks_alphas.append(np.array([alpha_crack]))
-    cracks_phis.append(np.array([phimax]))
+    # Creating lists of inclusion configurations (alpha and volume for each inclusion type)
+    crackandref_alphas = []
+    crackandref_volumes = []
+    stiffandref_alphas = []
+    stiffandref_volumes = []
+    fractions = [0.2, 0.4, 0.6, 0.8]
+    for fraction in fractions:
+        crackandref_alphas.append([alpha_crack, alpha_ref])
+        crackandref_volumes.append([fraction*phimax, (1.0-fraction)*phimax])
+        stiffandref_alphas.append([alpha_stiff, alpha_ref])
+        stiffandref_volumes.append([fraction*phimax, (1.0-fraction)*phimax])
     
-    ref_alphas = [np.array([alpha_ref])]
-    ref_phis = [np.array([phimax])]
+    alphas = [[alpha_crack]] + crackandref_alphas + [[alpha_ref]] + stiffandref_alphas + [[alpha_stiff]]
+    volumes = [[phimax]] + crackandref_volumes + [[phimax]] + stiffandref_volumes + [[phimax]]
     
-    stiffs_alphas = []
-    stiffs_phis = []
-    for frac in [0.2, 0.4, 0.6, 0.8]:
-        stiffs_alphas.append(np.array([alpha_stiff, alpha_ref]))
-        stiffs_phis.append(np.array([frac*phimax, (1.0-frac)*phimax]))
-    stiffs_alphas.append(np.array([alpha_stiff]))
-    stiffs_phis.append(np.array([phimax]))
+    # Adding one extra element for the dolomite line
+    alphas += [[alpha_ref]]
+    volumes += [[phimax]]
     
-    plt.figure(figsize=(7.0, 14.0/3.0), dpi=150)
-    plt.subplots_adjust(left = 0.12, top = 0.88, bottom = 0.15, right = 0.9)
-    # plt.figure(figsize=(7.0, 15.4/3.0), dpi=150)
-    # plt.subplots_adjust(left = 0.11, top = 0.88, bottom = 0.16, right = 0.96)
+    # Creating lists of matrix proeprties
+    Kms = len(alphas)*[Km_lim]
+    Gms = len(alphas)*[Gm_lim]
+    Kms[-1] = Km_dol
+    Gms[-1] = Gm_dol
+    
+    # Creating style for each line
+    crack_style = 'm:'
+    ref_style = 'b-'
+    stiff_style = 'r--'
+    dol_style = 'g-'
+    
+    styles = [crack_style] + [crack_style]*len(crackandref_alphas) + [ref_style] + [stiff_style] + [stiff_style]*len(stiffandref_alphas)
+    styles += [dol_style]
+    
+    # Plot configuration
+    plt.figure(figsize=(7.0, 15.4/3.0), dpi=150)
+    plt.subplots_adjust(left = 0.11, top = 0.88, bottom = 0.16, right = 0.96)
     plt.subplot(1, 1, 1)
     
-    allalphas = cracks_alphas + ref_alphas + stiffs_alphas
-    allphis = cracks_phis + ref_phis + stiffs_phis
-    styles = ['m:']*len(cracks_alphas) + ['b-']*len(ref_alphas) + ['r--']*len(stiffs_alphas)
-    
-    for alphas, phis, style in zip(allalphas, allphis, styles):
-        ni = len(alphas)
-        K, G, phi = DEM(Km_lim, Gm_lim, np.zeros(ni, dtype=float), np.zeros(ni, dtype=float), alphas, phis)
-        phistep = phi[1:] - phi[:-1]
-        print phi[0], phistep[0]/phistep[-1], len(phistep)
+    for inclusion_alphas, inclusion_volumes, style, Km, Gm in zip(alphas, volumes, styles, Kms, Gms):
+        ni = len(inclusion_alphas)
+        
+        # Dry inclusions
+        Kis = np.zeros(ni, dtype=float)
+        Gis = np.zeros(ni, dtype=float)
+
+        K, G, phi = DEM(Km, Gm, Kis, Gis, np.array(inclusion_alphas), np.array(inclusion_volumes))
         
         rho = (1.0 - phi)*rhom_lim + phi*rhof
-        Ks = Gassmann.Ks(K, Km_lim, Kf, phi)
+        Ks = Gassmann.Ks(K, Km, Kf, phi)
         
         Vp = np.sqrt((Ks + 4.0*G/3.0)/rho)
-        Vs = np.sqrt(G/rho)
         
         plt.plot(phi, Vp, style)
     
-    for alphas, phis in zip(ref_alphas, ref_phis):
-        ni = len(alphas)
-        K, G, phi = DEM(Km_dol, Gm_dol, np.zeros(ni, dtype=float), np.zeros(ni, dtype=float), alphas, phis)
-        
-        rho = (1.0 - phi)*rhom_lim + phi*rhof
-        Ks = Gassmann.Ks(K, Km_lim, Kf, phi)
-        
-        Vp = np.sqrt((Ks + 4.0*G/3.0)/rho)
-        Vs = np.sqrt(G/rho)
-        
-        plt.plot(phi, Vp, 'g-')
-    
-    # data = np.loadtxt("data.txt")
-    # vplog, philog, litolog = data.T
-    
-    # plt.scatter(philog[litolog == 1.0], vplog[litolog == 1.0], c='b', s=5.0, zorder=-1000, linewidths=0.25)
-    # plt.scatter(philog[litolog == 30.0], vplog[litolog == 30.0], c='g')
-    
     plt.ylabel("$V_p (km/s)$", fontsize=20)
     plt.xlabel("$\\phi$", fontsize=20)
-    # plt.ylim(2.0, 7.0)
-    plt.ylim(1.0, 7.0)
+    plt.ylim(2.0, 7.0)
     plt.xlim(0.0, phimax)
     
     plt.grid()
-    plt.savefig("XuPayne1a.png", dpi=150)
-    plt.show()
+    plt.savefig("exampleXuPayne2009.png", dpi=150)
 
 if __name__ == '__main__':
     XuPayne()
